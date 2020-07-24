b9e8f4f9640b6c86bec280629204564c2db7f060
Patrick O'Connell <poconnell6@student.cvtc.edu>
2020-07-24 15:32:36 -0500

    after Add-Migration added-pricing-to-books

---
+++ Migrations/202007242020099_added-pricing-to-books.cs
@@ -0,0 +1,20 @@
+﻿namespace SC_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedpricingtobooks : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.books", "Price", c => c.Decimal(nullable: false, storeType: "money"));
+            AddColumn("dbo.books", "PercentDiscountedTo", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.books", "PercentDiscountedTo");
+            DropColumn("dbo.books", "Price");
+        }
+    }
+}
--- Models/Work.cs
+++ Models/Work.cs
@@ -15,6 +15,12 @@ namespace SC_MVC.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "Money")]
+        public decimal Price { get; set; }
+        public int PercentDiscountedTo { get; set; }
+
+
         public bool IsOnSale { get; set; }
     }
 
